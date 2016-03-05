c26ab9f7093a525f22ce759f8f9461ba028709e1
mppereiram <mppereiram@mcloud.cl>
2016-03-05 00:59:26 -0300

    Tarea 1

---
+++ Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for i in 0...100{
+    
+    if (i % 5 == 0){
+        print("# \(i) Bingo!!!")
+    } else if (i % 2 == 0){
+        print("# \(i) par!!!")
+    } else if (i % 2 != 0){
+        print("# \(i) impar!!!")
+    }
+        
+    if (i >= 30 && i <= 40){
+        print("# \(i) Viva Swift!!!")
+    }
+    
+}
